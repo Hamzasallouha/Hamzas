00b8043bea158f8ac5b6073e275390c7055779ed
Hamza sallouha <hamzasallouha@gmail.com>
2023-03-25 11:50:41 +0300

    first update

---
+++ src/hamza.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hamza {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("welcome hamza");
+        System.out.println("enter the since");
+        int since = scanner .nextInt();
+        int age =2023- since;
+        System.out.println("this is since "+ since);
+        System.out.println("The age" + age);
+    }
+}
